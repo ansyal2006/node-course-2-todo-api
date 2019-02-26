ed0ac12c4e084d9964d9fa4c29e821c732c084ab
Anshul Goyal <ansyal2006@gmail.com>
2019-02-26 13:30:32 +0530

    =Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
+const todos = [{text : 'First todo'},{text : 'Second todo'}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    //insertMany function adds elements of an array to a collection and returns a promise on which another then() call is made.
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
-describe('POST /Todos', () => {
+describe('POST /todos', () => {
 
   it('should create a new todo', (done) => {
 
@@ -26,7 +31,7 @@ describe('POST /Todos', () => {
         return done(err);
       }
 
-      Todo.find().then((todos) => {
+      Todo.find({text : text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -49,10 +54,21 @@ describe('POST /Todos', () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
   });
+});
 
+describe('GET /todos', () => {
+  it('should get all the todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  })
 });
